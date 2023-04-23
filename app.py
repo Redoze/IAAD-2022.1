3ba40c750292dabf2ae33226730f6e8d13dbf1d9
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-22 23:10:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,34 +3,23 @@ from consultar import *
 from cadastrar import *
 from atualizar import *
 from remover import *
-from criar import *
-try:
-    st.set_page_config(
-        page_title="Operações CRUD",
-        page_icon="📄"
-    )
 
-    def main():
+def main():
+    
+        st.sidebar.title('Operações CRUD')
+        st.sidebar.markdown("---")
         
-            st.sidebar.title('Operações CRUD')
-            st.sidebar.markdown("---")
-            
-            tabs = ["Consultar", "Cadastrar", "Atualizar", "Remover", "Criar"]
-            selected_tab = st.sidebar.radio("Selecione a operação que deseja realizar", tabs)
+        tabs = ["Consultar", "Cadastrar", "Atualizar", "Remover"]
+        selected_tab = st.sidebar.radio("Selecione a operação que deseja realizar", tabs)
 
-            if selected_tab == "Consultar":
-                consultar()
-            elif selected_tab == "Cadastrar":
-                cadastrar()
-            elif selected_tab == "Remover":
-                remover()
-            elif selected_tab == "Atualizar":
-                atualizar()
-            elif selected_tab == "Criar":
-                criar()
+        if selected_tab == "Consultar":
+            consultar()
+        elif selected_tab == "Cadastrar":
+            cadastrar()
+        elif selected_tab == "Remover":
+            remover()
+        elif selected_tab == "Atualizar":
+            atualizar()
 
-    if __name__ == "__main__":
-        main()
-
-except st.StreamlitAPIException:
-    st.warning('Por favor, atualize a página')
+if __name__ == "__main__":
+    main()
