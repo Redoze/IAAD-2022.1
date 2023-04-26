5bba80d166ad47932f15f9db63a41869a523b46c
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-25 21:56:01 -0300

    Update atualizar.py

--- atualizar.py
+++ atualizar.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from app import *
 from funcs import *
+import time
 
 def atualizar():
 
@@ -10,146 +10,166 @@ def atualizar():
     aba1, aba2 = st.tabs(["Medico", "Especialidade"])
 
     with aba1:
+
+        coluna1, coluna2 = st.columns(2)
+
+        with coluna1:
         
-        def atualizar_medico(input_field, input_value, input_Cod):
-            query = f"UPDATE medico SET {input_field} = %s WHERE CodMed = %s"
-            values = (input_value, input_Cod)
-            cursor.execute(query, values)
-            cnx.commit()
-            cursor.close()
-            cnx.close()
-            st.success("Médico alterado com sucesso!")
-            st.info(f'Código do Médico: {input_Cod}')
-            st.info(f'Campo alterado: {input_field}')
-            st.info(f'Novo valor: {input_value}')
+            def atualizar_medico(input_field, input_value, input_Cod):
+                query = f"UPDATE medico SET {input_field} = %s WHERE CodMed = %s"
+                values = (input_value, input_Cod)
+                cursor.execute(query, values)
+                cnx.commit()
+                cursor.close()
+                cnx.close()
+                st.success("Médico alterado com sucesso!")
+                st.info(f'Código do Médico: {input_Cod}')
+                st.info(f'Campo alterado: {input_field}')
+                st.info(f'Novo valor: {input_value}')
 
-        st.subheader('Atualizar Médico')
+                st.warning('Atualizando página')
+                with st.spinner('Aguarde a atualização da página para efetuar uma nova operação'):
+                    time.sleep(10)
+                st.experimental_rerun()
 
-        cnx = create_connection()
-        cursor = cnx.cursor()
-        cursor.execute("SELECT CodMed FROM medico")
-        resultado = cursor.fetchall()
-        valores = []
+            st.subheader('Atualizar Médico')
 
-        for i in resultado:
-            valores.append(i[0])
+            cnx = create_connection()
+            cursor = cnx.cursor()
+            cursor.execute("SELECT CodMed FROM medico")
+            resultado = cursor.fetchall()
+            valores = []
 
-        with st.form(key="update_medico"):
-            input_Cod = st.selectbox(label="Insira o Código do Médico a ser alterado", options = valores)
-            input_field = st.selectbox(label="Selecione o campo a ser alterado", options=["Código do Médico","Nome do Médico", "Gênero", "Telefone", "Email", "Código da especialidade"])
-            input_value = st.text_input(label="Insira o novo valor")
-            input_button_submit = st.form_submit_button('Atualizar')
+            for i in resultado:
+                valores.append(i[0])
 
-        if input_button_submit:
+            with st.form(key="update_medico"):
+                input_Cod = st.selectbox(label="Insira o Código do Médico a ser alterado", options = valores)
+                input_field = st.selectbox(label="Selecione o campo a ser alterado", options=["Código do Médico","Nome do Médico", "Gênero", "Telefone", "Email", "Código da especialidade"])
+                input_value = st.text_input(label="Insira o novo valor")
+                input_button_submit = st.form_submit_button('Atualizar')
 
-            if input_field == "Código do Médico":
-                input_field = "CodMed"
+            if input_button_submit:
 
-                if len(input_value) == 7:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif len(input_value) != 7:
-                    st.warning("Insira 7 caracteres para o Código Médico")
+                if input_field == "Código do Médico":
+                    input_field = "CodMed"
 
-            if input_field == "Nome do Médico":
-                input_field = "NomeMed"
+                    if len(input_value) == 7:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif len(input_value) != 7:
+                        st.warning("Insira 7 caracteres para o Código Médico")
 
-                if len(input_value) > 0 and len(input_value) <= 40:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif len(input_value) == 0:
-                    st.warning('Você deve inserir o Nome do Médico')
-                elif len(input_value) > 15:
-                    st.warning('Você deve inserir até no máximo 40 caracteres para o Nome do Médico')
+                if input_field == "Nome do Médico":
+                    input_field = "NomeMed"
 
-            if input_field == "Gênero":
-                input_field = "Genero"
-                input_value = input_value.upper()
+                    if len(input_value) > 0 and len(input_value) <= 40:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif len(input_value) == 0:
+                        st.warning('Você deve inserir o Nome do Médico')
+                    elif len(input_value) > 15:
+                        st.warning('Você deve inserir até no máximo 40 caracteres para o Nome do Médico')
 
-                if input_value == 'M' or input_value == 'F' or len(input_value) == 0:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif input_value != 'M' or input_value != 'F':
-                    st.warning('Você deve abreviar o gênero apenas para as letras "M" ou "F"')
+                if input_field == "Gênero":
+                    input_field = "Genero"
+                    input_value = input_value.upper()
 
-            if input_field == "Telefone":
+                    if input_value == 'M' or input_value == 'F' or len(input_value) == 0:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif input_value != 'M' or input_value != 'F':
+                        st.warning('Você deve abreviar o gênero apenas para as letras "M" ou "F"')
 
-                if len(input_value) <= 16:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif len(input_value) > 16:
-                    st.warning("Insira no máximo 16 caracteres para o Número do Telefone")
+                if input_field == "Telefone":
 
-            if input_field == "Email":
+                    if len(input_value) <= 16:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif len(input_value) > 16:
+                        st.warning("Insira no máximo 16 caracteres para o Número do Telefone")
 
-                if len(input_value) > 0 and len(input_value) <=40:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif len(input_value) == 0:
-                    st.warning('Você deve inserir o Email')
-                elif len(input_value) > 40:
-                    st.warning('Você deve inserir até no máximo 40 caracteres para o Email')
+                if input_field == "Email":
 
-            if input_field == "Código da especialidade":
-                input_field = "CodEspec"
+                    if len(input_value) > 0 and len(input_value) <=40:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif len(input_value) == 0:
+                        st.warning('Você deve inserir o Email')
+                    elif len(input_value) > 40:
+                        st.warning('Você deve inserir até no máximo 40 caracteres para o Email')
 
-                if len(input_value) == 7:
-                    atualizar_medico(input_field, input_value, input_Cod)
-                elif len(input_value) != 7:
-                    st.warning("Insira 7 caracteres para o Código da Especialidade")
+                if input_field == "Código da especialidade":
+                    input_field = "CodEspec"
 
-        tabela_bd('Médicos', 'medico')
+                    if len(input_value) == 7:
+                        atualizar_medico(input_field, input_value, input_Cod)
+                    elif len(input_value) != 7:
+                        st.warning("Insira 7 caracteres para o Código da Especialidade")
+
+        with coluna2:
+            tabela_bd('Médicos', 'medico')
 
     with aba2:
 
-        def atualizar_especialidade(input_field, input_value, input_Cod):
-            query = f"UPDATE especialidade SET {input_field} = %s WHERE CodEspec = %s"
-            values = (input_value, input_Cod)
-            cursor.execute(query, values)
-            cnx.commit()
-            cursor.close()
-            cnx.close()
-            st.success(f'Especialidade {input_Cod} alterada com sucesso!')
-            st.info(f'Campo alterado: {input_field}')
-            st.info(f'Novo valor: {input_value}')
+        coluna1, coluna2 = st.columns(2)
 
-        st.subheader('Atualizar Especialidade')
+        with coluna1:
 
-        cnx = create_connection()
-        cursor = cnx.cursor()
-        cursor.execute("SELECT CodEspec FROM especialidade")
-        resultado = cursor.fetchall()
-        valores = []
+            def atualizar_especialidade(input_field, input_value, input_Cod):
+                query = f"UPDATE especialidade SET {input_field} = %s WHERE CodEspec = %s"
+                values = (input_value, input_Cod)
+                cursor.execute(query, values)
+                cnx.commit()
+                cursor.close()
+                cnx.close()
+                st.success(f'Especialidade {input_Cod} alterada com sucesso!')
+                st.info(f'Campo alterado: {input_field}')
+                st.info(f'Novo valor: {input_value}')
 
-        for i in resultado:
-            valores.append(i[0])
-        
-        with st.form(key="update_especialidade"):
-            input_Cod = st.selectbox(label="Insira o Código da Especialidade a ser alterada", options = valores)
-            input_field = st.selectbox(label="Selecione o campo a ser alterado", options=["Código da especialidade","Nome da especialidade", "Descrição"])
-            input_value = st.text_input(label="Insira o novo valor")
-            input_button_submit = st.form_submit_button('Atualizar')
+                st.warning('Atualizando página')
+                with st.spinner('Aguarde a atualização da página para efetuar uma nova operação'):
+                    time.sleep(10)
+                st.experimental_rerun()
 
-        if input_button_submit:
-            if input_field == "Código da especialidade":
-                input_field = "CodEspec"
+            st.subheader('Atualizar Especialidade')
 
-                if len(input_value) == 7:
-                    atualizar_especialidade(input_field, input_value, input_Cod)
-                elif len(input_value) != 7:
-                    st.warning("Insira 7 caracteres para o Código da Especialidade")
+            cnx = create_connection()
+            cursor = cnx.cursor()
+            cursor.execute("SELECT CodEspec FROM especialidade")
+            resultado = cursor.fetchall()
+            valores = []
 
-            if input_field == "Nome da especialidade":
-                input_field = "NomeEspec"
+            for i in resultado:
+                valores.append(i[0])
+            
+            with st.form(key="update_especialidade"):
+                input_Cod = st.selectbox(label="Insira o Código da Especialidade a ser alterada", options = valores)
+                input_field = st.selectbox(label="Selecione o campo a ser alterado", options=["Código da especialidade","Nome da especialidade", "Descrição"])
+                input_value = st.text_input(label="Insira o novo valor")
+                input_button_submit = st.form_submit_button('Atualizar')
 
-                if len(input_value) > 0 and len(input_value) <=15:
-                    atualizar_especialidade(input_field, input_value, input_Cod)
-                elif len(input_value) == 0:
-                    st.warning('Você deve inserir o Nome da Especialidade')
-                elif len(input_value) > 15:
-                    st.warning('Você deve inserir até no máximo 15 caracteres para o Nome da Especialidade')
+            if input_button_submit:
+                if input_field == "Código da especialidade":
+                    input_field = "CodEspec"
 
-            if input_field == "Descrição":
-                input_field = "Descricao"
+                    if len(input_value) == 7:
+                        atualizar_especialidade(input_field, input_value, input_Cod)
+                    elif len(input_value) != 7:
+                        st.warning("Insira 7 caracteres para o Código da Especialidade")
 
-                if len(input_value) > 0:
-                    atualizar_especialidade(input_field, input_value, input_Cod)
-                elif len(input_value) == 0:
-                    st.warning('Você deve inserir uma Descrição para a Especialidade')
+                if input_field == "Nome da especialidade":
+                    input_field = "NomeEspec"
 
-        tabela_bd('Especialidade', 'especialidade')
+                    if len(input_value) > 0 and len(input_value) <=15:
+                        atualizar_especialidade(input_field, input_value, input_Cod)
+                    elif len(input_value) == 0:
+                        st.warning('Você deve inserir o Nome da Especialidade')
+                    elif len(input_value) > 15:
+                        st.warning('Você deve inserir até no máximo 15 caracteres para o Nome da Especialidade')
+
+                if input_field == "Descrição":
+                    input_field = "Descricao"
+
+                    if len(input_value) > 0:
+                        atualizar_especialidade(input_field, input_value, input_Cod)
+                    elif len(input_value) == 0:
+                        st.warning('Você deve inserir uma Descrição para a Especialidade')
+
+        with coluna2:
+            tabela_bd('Especialidade', 'especialidade')
