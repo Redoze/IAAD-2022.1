bcb6bdd65ff1bcf471f4d39727a32616fb329841
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-25 21:55:17 -0300

    Update cadastrar.py

--- cadastrar.py
+++ cadastrar.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from app import *
 from funcs import *
+import time
 
 def cadastrar():
     st.header("Operação de Cadastro")
@@ -10,78 +10,100 @@ def cadastrar():
     
     with aba1:
 
-        st.subheader("Cadastrar Médico")
+        coluna1, coluna2 = st.columns(2)
 
-        cnx = create_connection()
-        cursor = cnx.cursor()
-        cursor.execute("SELECT CodEspec FROM especialidade")
-        resultado = cursor.fetchall()
-        valores = []
+        with coluna1:
 
-        for i in resultado:
-            valores.append(i[0])
+            st.subheader("Cadastrar Médico")
 
-        with st.form(key="include_medico"):
-            input_CodMed = st.text_input(label="Insira o Código do Médico", help = "insira um valor númerico de 7 digitos", max_chars = 7)
-            input_name = st.text_input(label="Insira o nome do Médico", max_chars = 40)
-            input_genero = st.selectbox(label="Selecione seu Gênero", options=["M", "F"])
-            input_telefone = st.text_input(label="Insira o Telefone", max_chars = 16)
-            input_email = st.text_input(label="Insira o Email", max_chars = 40)
-            input_CodEspec = st.selectbox(label="Insira o Código da Especialidade", options = valores)
-            input_button_submit = st.form_submit_button('Cadastrar')
+            cnx = create_connection()
+            cursor = cnx.cursor()
+            cursor.execute("SELECT CodEspec FROM especialidade")
+            resultado = cursor.fetchall()
+            valores = []
 
-        if input_button_submit:
-            if len(input_CodMed) == 7 and len(input_name) != 0:
-                query = "INSERT INTO Medico (CodMed, NomeMed, Genero, Telefone, Email, CodEspec) VALUES (%s, %s, %s, %s, %s, %s)"
-                values = (input_CodMed, input_name, input_genero, input_telefone, input_email, input_CodEspec)
-                cursor.execute(query, values)
-                cnx.commit()
-                cursor.close()
-                cnx.close()
+            for i in resultado:
+                valores.append(i[0])
 
-                st.success("Médico adicionado com sucesso!")
-                st.info(f'Código do Médico: {input_CodMed}')
-                st.info(f'Médico: {input_name}')
-                st.info(f'Gênero: {input_genero}')
-                st.info(f'Telefone: {input_telefone}')
-                st.info(f'Email: {input_email}')
-                st.info(f'Código da Especialidade: {input_CodEspec}')
-                
-            elif len(input_CodMed) < 7:
-                st.warning('Você deve inserir 7 caracteres para o Código do Médico')
-            elif len(input_name) == 0:
-                st.warning('Você deve inserir o Nome do Médico')
+            with st.form(key="include_medico"):
+                input_CodMed = st.text_input(label="Insira o Código do Médico", help = "Insira um valor númerico de 7 digitos", max_chars = 7)
+                input_name = st.text_input(label="Insira o nome do Médico", max_chars = 40)
+                input_genero = st.selectbox(label="Selecione seu Gênero", options=["M", "F"])
+                input_telefone = st.text_input(label="Insira o Telefone", max_chars = 16)
+                input_email = st.text_input(label="Insira o Email", max_chars = 40)
+                input_CodEspec = st.selectbox(label="Insira o Código da Especialidade", options = valores)
+                input_button_submit = st.form_submit_button('Cadastrar')
 
-        tabela_bd('Médicos', 'medico')
+            if input_button_submit:
+                if len(input_CodMed) == 7 and len(input_name) != 0:
+
+                    query = "INSERT INTO Medico (CodMed, NomeMed, Genero, Telefone, Email, CodEspec) VALUES (%s, %s, %s, %s, %s, %s)"
+                    values = (input_CodMed, input_name, input_genero, input_telefone, input_email, input_CodEspec)
+                    cursor.execute(query, values)
+                    cnx.commit()
+                    cursor.close()
+                    cnx.close()
+
+                    st.success("Médico adicionado com sucesso!")
+                    st.info(f'Código do Médico: {input_CodMed}')
+                    st.info(f'Médico: {input_name}')
+                    st.info(f'Gênero: {input_genero}')
+                    st.info(f'Telefone: {input_telefone}')
+                    st.info(f'Email: {input_email}')
+                    st.info(f'Código da Especialidade: {input_CodEspec}')
+
+                    st.warning('Atualizando página')
+                    with st.spinner('Aguarde a atualização da página para efetuar uma nova operação'):
+                        time.sleep(10)
+                    st.experimental_rerun()
+
+                elif len(input_CodMed) < 7:
+                    st.warning('Você deve inserir 7 caracteres para o Código do Médico')
+                elif len(input_name) == 0:
+                    st.warning('Você deve inserir o Nome do Médico')
+
+        with coluna2:
+            tabela_bd('Médicos', 'medico')
         
     with aba2:
 
-        st.subheader("Cadastrar Especilidade")
+        coluna1, coluna2 = st.columns(2)
 
-        with st.form(key="include_especialidade"):
-            input_CodEsp = st.text_input(label="Insira o Código da Especialidade", help = "insira um valor númerico de 7 digitos", max_chars = 7)
-            input_name = st.text_input(label="Insira o nome da Especialidade", max_chars = 15)
-            input_descricao = st.text_input(label="Descrição da Especialidade")
-            input_button_submit = st.form_submit_button('Cadastrar')
+        with coluna1:
 
-        if input_button_submit:
-            if len(input_CodEsp) == 7 and len(input_name) and len(input_descricao)!= 0:
-                query = "INSERT INTO especialidade (CodEspec, NomeEspec, Descricao) VALUES (%s, %s, %s)"
-                values = (input_CodEsp, input_name, input_descricao)
-                cursor.execute(query, values)
-                cnx.commit()
-                cursor.close()
-                cnx.close()
+            st.subheader("Cadastrar Especilidade")
 
-                st.success("Especialidade adicionada com sucesso!")
-                st.info(f'Código Especialidade: {input_CodEsp}')
-                st.info(f'Nome: {input_name}')
-                st.info(f'Descrição: {input_descricao}')
+            with st.form(key="include_especialidade"):
+                input_CodEsp = st.text_input(label="Insira o Código da Especialidade", help = "Insira um valor númerico de 7 digitos", max_chars = 7)
+                input_name = st.text_input(label="Insira o nome da Especialidade", max_chars = 15)
+                input_descricao = st.text_input(label="Descrição da Especialidade")
+                input_button_submit = st.form_submit_button('Cadastrar')
+
+            if input_button_submit:
+                if len(input_CodEsp) == 7 and len(input_name)!= 0 and len(input_descricao)!= 0:
+
+                    query = "INSERT INTO especialidade (CodEspec, NomeEspec, Descricao) VALUES (%s, %s, %s)"
+                    values = (input_CodEsp, input_name, input_descricao)
+                    cursor.execute(query, values)
+                    cnx.commit()
+                    cursor.close()
+                    cnx.close()
+
+                    st.success("Especialidade adicionada com sucesso!")
+                    st.info(f'Código Especialidade: {input_CodEsp}')
+                    st.info(f'Nome: {input_name}')
+                    st.info(f'Descrição: {input_descricao}')
+
+                    with st.spinner('Aguarde a atualização da página para efetuar uma nova operação'):
+                            time.sleep(10)
+                    st.experimental_rerun()
                 
-            elif len(input_CodEsp) < 7:
-                st.warning('Você deve inserir 7 caracteres para o Código da Especialidade')
-            elif len(input_name) == 0:
-                st.warning('Você deve inserir o Nome da Especialidade')
-            elif len(input_descricao) == 0:
-                st.warning('Você deve inserir uma Descrição para a Especialidade')
-        tabela_bd('Especialidade', 'especialidade')
+                elif len(input_CodEsp) < 7:
+                    st.warning('Você deve inserir 7 caracteres para o Código da Especialidade')
+                elif len(input_name) == 0:
+                    st.warning('Você deve inserir o Nome da Especialidade')
+                elif len(input_descricao) == 0:
+                    st.warning('Você deve inserir uma Descrição para a Especialidade')
+                    
+        with coluna2:
+            tabela_bd('Especialidade', 'especialidade')
