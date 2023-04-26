f36d7d63df03815d1a9188efd7828bd3a8634ddf
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-25 21:57:36 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,37 +1,28 @@
 import streamlit as st
 import mysql.connector
-from app import *
+import pandas as pd
 
 def create_connection():                                        
     cnx = mysql.connector.connect(user='root',                  #########################################################################
-                                password='SUA SENHA',           ########## ADICIONE SEUS DADOS DE CONEXÃO COM O BANCO DE DADOS ##########
+                                password='iaadtask',           ########## ADICIONE SEUS DADOS DE CONEXÃO COM O BANCO DE DADOS ##########
                                 host='localhost',               #########################################################################
                                 database='clinicasmedicas')
     return cnx
 
 def tabela_bd (nome_tabela_plural, nome_tabela):
 
-    st.subheader(nome_tabela_plural)
+    st.subheader('Tabela ' + nome_tabela_plural)
 
     cnx = create_connection()
     cursor = cnx.cursor()
     query = "SELECT * FROM " + nome_tabela
-    cursor.execute(query)
-    resultado = cursor.fetchall()
-    st.dataframe(resultado)
+    df = pd.read_sql_query(query, cnx)
+    if nome_tabela == 'medico':
+        df.columns = ["Código do Médico", "Nome do Médico", "Gênero", 'Telefone',' Email','Código de especialidade']
+    if nome_tabela == 'especialidade':
+        df.columns = ["Código de especialidade", "Nome da especialidade", "Descrição da especialidade"]
+    
+    st.dataframe(df)
     cnx.commit()
     cursor.close()
     cnx.close()
-
-    if nome_tabela == 'medico':
-        st.caption('Coluna 0 = Código do Médico')
-        st.caption("Coluna 1 = Nome do Médico")
-        st.caption("Coluna 2 = Gênero")
-        st.caption("Coluna 3 = Telefone")
-        st.caption("Colune 4 = Email")
-        st.caption("Coluna 5 = Código de especialidade")
-        
-    if nome_tabela == 'especialidade':
-        st.caption('Coluna 0 = Código de especialidade')
-        st.caption("Coluna 1 = Nome da especialidade")
-        st.caption("Coluna 2 = Descrição da especialidade")
